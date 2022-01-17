8b55987e2ab030732f5f0e9d233492eb58c4dded
Cubxity <contact@cubxity.dev>
2022-01-17 19:42:40 +0100

    chore: use preact for production build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 module.exports = {
-  webpack(config) {
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+      });
+    }
+
     config.module.rules.push({
       test: /\.svg$/,
       use: [
         {
           loader: "@svgr/webpack",
-          options: { dimensions: false }
-        }
-      ]
+          options: { dimensions: false },
+        },
+      ],
     });
 
     return config;
-  }
+  },
 };
