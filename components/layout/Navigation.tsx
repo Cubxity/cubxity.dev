b1ac3696483cfcbc3fe6bef231e3c5a5f12869de
Cubxity <cubxity@protonmail.com>
2022-01-18 15:40:56 +0100

    feat: improve navigation structure

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -4,9 +4,15 @@ import Link from "../Link";
 
 export default function Navigation() {
   return (
-    <nav className="container flex flex-row items-center gap-8 w-full mx-auto px-4 py-8">
-      <Link href="#">Projects</Link>
-      <Link href="#">Blog</Link>
+    <nav className="container w-full mx-auto px-4 py-8">
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <Link href="#">Projects</Link>
+        </li>
+        <li>
+          <Link href="#">Blog</Link>
+        </li>
+      </ul>
       <div className="grow" />
       {/*<NextLink href="#" passHref>
         <a className="font-xl font-semibold text-white rounded-full border-2 border-white px-8 py-2">
