3fb1bde93308f05f01ee9f59c55b2ce82d63b76c
Cubxity <cubxity@protonmail.com>
2022-01-18 07:54:58 +0100

    feat: socials

--- components/layout/Socials.tsx
+++ components/layout/Socials.tsx
@@ -1,12 +1,24 @@
 import GitHubIcon from "../../assets/svg/logo/fa-github.svg";
+import TwitterIcon from "../../assets/svg/logo/fa-twitter.svg";
+import DiscordIcon from "../../assets/svg/logo/fa-discord.svg";
 import IconButton from "../IconButton";
 
 export default function Socials() {
   return (
-    <div className="pt-4">
+    <div className="py-4 space-x-4">
       <IconButton
         icon={GitHubIcon}
         href="https://github.com/Cubxity"
+        external
+      />
+      <IconButton
+        icon={DiscordIcon}
+        href="https://discord.gg/UUPeTMVjV7"
+        external
+      />
+      <IconButton
+        icon={TwitterIcon}
+        href="https://twitter.com/Cubxity"
         external
       />
     </div>
