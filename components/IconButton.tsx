15ac794967bfc121440b91ab975c4254b58b2852
Cubxity <cubxity@protonmail.com>
2022-01-19 07:46:07 +0100

    feat: improve IconButton

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -11,31 +11,36 @@ export interface IconButtonProps {
 export default function IconButton(props: IconButtonProps) {
   const { icon: Icon, href, external, onClick } = props;
 
-  const button = (
+  if (href) {
+    const a = (
+      <a
+        href={href}
+        rel="noreferrer"
+        target="_blank"
+        className="p-2 w-min h-min rounded-full transition-colors hover:bg-white/20 inline-block"
+        onClick={onClick}
+      >
+        <Icon className="text-white w-6 h-6" />
+      </a>
+    );
+
+    if (external) {
+      return a;
+    } else {
+      return (
+        <Link href={href} passHref>
+          {a}
+        </Link>
+      );
+    }
+  }
+
+  return (
     <button
-      type="button"
       className="p-2 w-min h-min rounded-full transition-colors hover:bg-white/20"
       onClick={onClick}
     >
       <Icon className="text-white w-6 h-6" />
     </button>
   );
-
-  if (href) {
-    if (external) {
-      return (
-        <a href={href} rel="noreferrer" target="_blank">
-          {button}
-        </a>
-      );
-    } else {
-      return (
-        <Link href={href} passHref>
-          {button}
-        </Link>
-      );
-    }
-  }
-
-  return button;
 }
