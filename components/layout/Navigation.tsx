320c0ae396f9febd58b12dbaedb52e1c271b2137
Cubxity <cubxity@protonmail.com>
2022-01-20 19:49:39 +0100

    refactor: temporary remove navigation links

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-import Link from "../Link";
-
 export default function Navigation() {
   return (
     <nav className="container w-full mx-auto px-4 py-8">
       <ul className="flex flex-row items-center gap-8">
-        <li>
+        {/*<li>
           <Link href="#">Projects</Link>
         </li>
         <li>
           <Link href="#">Blog</Link>
-        </li>
+        </li>*/}
       </ul>
       <div className="grow" />
     </nav>
