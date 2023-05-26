eedbd639fe52a106f74422b889e82a31055b61f8
Cubxity <contact@cubxity.dev>
2023-05-26 22:15:16 +0200

    fix(build): dependencies and remove preact

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom": "preact/compat",
-      });
-    }
-
     config.module.rules.push({
       test: /\.svg$/,
       use: [
