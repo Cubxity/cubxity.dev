d58fc4f0d6cd3410ff662123c0ae5a3ca6862f59
Cubxity <cubxity@protonmail.com>
2023-06-25 00:46:54 +0200

    feat(components): improve contact button in navbar

--- src/app/components/layout/Navigation.tsx
+++ src/app/components/layout/Navigation.tsx
@@ -1,3 +1,4 @@
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 import NextLink from "next/link";
 import React from "react";
@@ -35,8 +36,9 @@ export default function Navigation() {
         <NextLink
           href="/contact"
           role="button"
-          className="rounded-full bg-indigo-600 px-4 py-2 text-sm font-medium text-white"
+          className="flex flex-row items-center gap-2 rounded-md border border-gray-700 hover:border-indigo-600 px-4 py-2 text-sm font-medium transition-all"
         >
+          <EnvelopeIcon className="w-5 h-5" />
           Contact
         </NextLink>
       </div>
