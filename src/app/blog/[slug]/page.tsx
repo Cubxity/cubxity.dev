2f1fcd3f506205f2643f744d31b38bef362cd497
Cubxity <cubxity@protonmail.com>
2023-06-25 00:19:10 +0200

    feat(pages): improve blog layout on mobile

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -17,7 +17,7 @@ const BlogPage = async ({ params }: BlogPageProps) => {
       <header className="mx-auto w-full max-w-screen-lg px-4 pt-24 md:pt-36 pb-4 md:w-5/6 md:pb-16 lg:w-2/3">
         <Link
           href="/blog"
-          className="text-gray-400 flex flex-row items-center gap-2 transition-colors hover:text-indigo-400 mb-8"
+          className="text-gray-400 flex flex-row items-center gap-2 transition-colors hover:text-indigo-400 mb-10"
         >
           <ArrowLeftIcon className="w-6 h-6" />
           Blog
@@ -26,10 +26,10 @@ const BlogPage = async ({ params }: BlogPageProps) => {
           <span className="text-md font-medium text-neutral-400 md:text-lg">
             {new Date(data.post.updatedAt).toDateString()}
           </span>
-          <h1 className="mt-4 mb-6 text-3xl font-bold text-white md:mb-8 md:text-6xl">
+          <h1 className="mt-4 mb-4 text-3xl font-bold text-white md:mb-8 md:text-6xl">
             {data.post.title}
           </h1>
-          <p className="text-xl text-neutral-200">
+          <p className="md:text-xl text-neutral-200">
             {data.post.meta.description}
           </p>
           {data.post.coverImage && (
